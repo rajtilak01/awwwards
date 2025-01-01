49c4a981c9dbcc94eb93ea500ab65f072ebe71a5
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2025-01-01 18:19:31 +0530

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import Hero from './components/Hero'
+import About from "./components/About";
+import Hero from "./components/Hero";
+import NavBar from "./components/Navbar";
+import Features from "./components/Features";
+import Story from "./components/Story";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
-  
-
   return (
-    <main>
-      <Hero/>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
