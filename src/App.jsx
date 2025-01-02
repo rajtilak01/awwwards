ab5cb66d731fb1f65709428cc5ddcdc8ce04bbf4
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2025-01-02 23:01:36 +0530

    missing package

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
-import NavBar from "./components/Navbar";
+import Navbar from "./components/Navbar"
 import Features from "./components/Features";
 import Story from "./components/Story";
 import Contact from "./components/Contact";
@@ -9,7 +9,7 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <NavBar />
+      <Navbar />
       <Hero />
       <About />
       <Features />
